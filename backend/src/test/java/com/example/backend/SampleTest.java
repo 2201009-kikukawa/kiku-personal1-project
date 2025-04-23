e4d944dde5d63953a3ce8106e805c601d7e04346
kikukawa <2201009@s.asojuku.ac.jp>
2025-04-23 10:12:27 +0900

    ci 実装

---
+++ backend/src/test/java/com/example/backend/SampleTest.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleTest {
+    @Test
+    public void testSomething() {
+        Assertions.assertTrue(true);
+    }
+}
